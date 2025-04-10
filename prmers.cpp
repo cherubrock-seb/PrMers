b713880a4202207ccef823d15dce9eee443da1fd
cherubrock-seb <iofabul@gmail.com>
2025-04-10 19:32:32 +0200

    Release prmers

--- prmers.cpp
+++ prmers.cpp
@@ -25,7 +25,11 @@
  * This code is released as free software.
  */
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
 #include <CL/cl.h>
+#endif
 #include <iostream>
 #include <fstream>
 #include <sstream>
