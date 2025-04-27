146cbdffa71f398c44f0310e4ae5d1f94d3e4eba
cherubrock-seb <fricadev@macbook-air-de-sebastien-2.home>
2025-04-27 21:31:17 +0200

    config file

--- src/math/Mod64.cpp
+++ src/math/Mod64.cpp
@@ -1,19 +1,31 @@
 #include "math/Mod64.hpp"
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
 namespace math {
 
 uint64_t mulModP(uint64_t a, uint64_t b) {
+    uint64_t hi, lo;
+
 #ifdef _MSC_VER
-    uint64_t hi, lo = _umul128(a, b, &hi);
+    lo = _umul128(a, b, &hi);
 #else
-    __uint128_t prod = ( __uint128_t )a * b;
-    uint64_t hi = uint64_t(prod >> 64), lo = uint64_t(prod);
+    __uint128_t prod = static_cast<__uint128_t>(a) * static_cast<__uint128_t>(b);
+    hi = static_cast<uint64_t>(prod >> 64);
+    lo = static_cast<uint64_t>(prod);
 #endif
-    uint32_t A = uint32_t(hi >> 32), B = uint32_t(hi & 0xFFFFFFFFULL);
-    uint64_t r = lo, old = r;
-    r += (uint64_t(B) << 32);
+
+    uint32_t A = static_cast<uint32_t>(hi >> 32);
+    uint32_t B = static_cast<uint32_t>(hi & 0xFFFFFFFFULL);
+
+    uint64_t r = lo;
+    uint64_t old = r;
+    r += (static_cast<uint64_t>(B) << 32);
     if (r < old) r += ((1ULL << 32) - 1ULL);
-    uint64_t sub = uint64_t(A) + uint64_t(B);
+
+    uint64_t sub = static_cast<uint64_t>(A) + static_cast<uint64_t>(B);
     r = (r >= sub ? r - sub : r + MOD_P - sub);
     return (r >= MOD_P ? r - MOD_P : r);
 }
