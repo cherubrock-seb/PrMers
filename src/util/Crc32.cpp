e12d1ef0f9d4ef8a549cc64cd782a5abfb7ef989
cherubrock-seb <fricadev@macbook-air-de-sebastien-2.home>
2025-04-27 22:10:30 +0200

    add a README.MD

--- src/util/Crc32.cpp
+++ src/util/Crc32.cpp
@@ -1,3 +1,24 @@
+/*
+ * Mersenne OpenCL Primality Test Host Code
+ *
+ * This code is inspired by:
+ *   - "mersenne.cpp" by Yves Gallot (Copyright 2020, Yves Gallot) based on
+ *     Nick Craig-Wood's IOCCC 2012 entry (https://github.com/ncw/ioccc2012).
+ *   - The Armprime project, explained at:
+ *         https://www.craig-wood.com/nick/armprime/
+ *     and available on GitHub at:
+ *         https://github.com/ncw/
+ *   - Yves Gallot (https://github.com/galloty), author of Genefer 
+ *     (https://github.com/galloty/genefer22), who helped clarify the NTT and IDBWT concepts.
+ *   - The GPUOwl project (https://github.com/preda/gpuowl), which performs Mersenne
+ *     searches using FFT and double-precision arithmetic.
+ * This code performs a Mersenne prime search using integer arithmetic and an IDBWT via an NTT,
+ * executed on the GPU through OpenCL.
+ *
+ * Author: Cherubrock
+ *
+ * This code is released as free software.
+ */
 #include "util/Crc32.hpp"
 #include <cstdint>
 #include <iostream>
