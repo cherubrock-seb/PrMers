1728b452b1ff0a96bfc7f9d94bcf9210807cf1a8
Valerii Huhnin <olympichek1@gmail.com>
2025-07-09 11:49:56 +0300

    add a variant of `computeCRC32` function for binary data

--- include/util/Crc32.hpp
+++ include/util/Crc32.hpp
@@ -1,6 +1,9 @@
 #ifndef CRC32_HPP
 #define CRC32_HPP
 #include <string>
-unsigned int computeCRC32(const std::string &data);
+#include <cstdint>
+#include <cstddef>
+uint32_t computeCRC32(const std::string &data);
+uint32_t computeCRC32(const void* data, size_t size);
 std::string toLower(const std::string &s);
 #endif
