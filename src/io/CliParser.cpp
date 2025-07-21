5e86bb0c6b9373299529b67093204ad0c385dca8
cherubrock-seb <iofabul@gmail.com>
2025-07-21 10:18:25 +0200

    skip self signed

--- src/io/CliParser.cpp
+++ src/io/CliParser.cpp
@@ -93,7 +93,7 @@ CliOptions CliParser::parse(int argc, char** argv ) {
         || std::strcmp(argv[i], "--version") == 0
         || std::strcmp(argv[i], "-version") == 0)
         {
-            std::cout << "prmers Release v4.4.0-alpha\n";
+            std::cout << "prmers Release v4.5.0-alpha\n";
             std::exit(EXIT_SUCCESS);
         }
     }
