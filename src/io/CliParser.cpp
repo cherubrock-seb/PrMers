8c3d69448caa35320e6763134e504e31aa074a55
cherubrock-seb <iofabul@gmail.com>
2025-07-22 16:56:24 +0200

    Gerbicz li check

--- src/io/CliParser.cpp
+++ src/io/CliParser.cpp
@@ -95,7 +95,7 @@ CliOptions CliParser::parse(int argc, char** argv ) {
         || std::strcmp(argv[i], "--version") == 0
         || std::strcmp(argv[i], "-version") == 0)
         {
-            std::cout << "prmers Release v4.5.0-alpha\n";
+            std::cout << "prmers Release v4.6.0-alpha\n";
             std::exit(EXIT_SUCCESS);
         }
     }
