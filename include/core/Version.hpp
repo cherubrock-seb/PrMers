3cf3e00d72a17ba030f3ec64d991b78de784929b
Valerii Huhnin <olympichek1@gmail.com>
2025-08-07 04:20:57 +0300

    print PrMers version in JSON results

---
+++ include/core/Version.hpp
@@ -0,0 +1,10 @@
+#ifndef VERSION_HPP
+#define VERSION_HPP
+
+#include <string>
+
+namespace core {
+    const std::string PRMERS_VERSION = "4.8.05-alpha";
+} // namespace core
+
+#endif // VERSION_HPP
