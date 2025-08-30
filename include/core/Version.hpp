a6eab68aa3653c18c66ad4bcacd02ca33a8ad40d
cherubrock-seb <iofabul@gmail.com>
2025-08-30 22:52:24 +0200

    Gerbicz li with restore back

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.9.11-alpha";
+    const std::string PRMERS_VERSION = "4.10.00-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
