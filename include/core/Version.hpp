04fb989722f114c207f729f43e4dffc4529b1ada
cherubrock-seb <iofabul@gmail.com>
2025-09-03 22:42:25 +0200

    -submit deactivate by default

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.10.20-alpha";
+    const std::string PRMERS_VERSION = "4.10.21-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
