1ffd0f85eb1d3b5d5dc319cb4ca64fafd6e5c027
cherubrock-seb <iofabul@gmail.com>
2025-09-10 14:52:34 +0200

    PM1 factoring Turbo new backend marin

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.10.33-alpha";
+    const std::string PRMERS_VERSION = "4.11.00-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
