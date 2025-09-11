d2c081a9f19a5fb274f1569b674b0e389bfc88c9
cherubrock-seb <iofabul@gmail.com>
2025-09-11 18:18:43 +0200

    LL Safe Mode with Gerbicz

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.12.01-alpha";
+    const std::string PRMERS_VERSION = "4.12.02-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
