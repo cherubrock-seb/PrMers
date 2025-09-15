a56062c09d3d3d79b7a75dd61e5cf08b2bff153b
cherubrock-seb <iofabul@gmail.com>
2025-09-15 07:10:07 +0200

    worktodo parser + delete pm1 ckpt

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.12.03-alpha";
+    const std::string PRMERS_VERSION = "4.12.04-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
