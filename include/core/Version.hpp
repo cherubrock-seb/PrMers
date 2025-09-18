ada665cf911a510a3bae7ce9c243077bacd2efae
cherubrock-seb <iofabul@gmail.com>
2025-09-18 13:46:50 +0200

    Backtrack gerbicz li in pm1

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.13.12-alpha";
+    const std::string PRMERS_VERSION = "4.13.13-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
