0d168570037358e737c1f4d20cdb40ec3f0fc43e
cherubrock-seb <iofabul@gmail.com>
2025-09-18 10:27:10 +0200

    P-1 Factoring with Gerbicz Li error checking

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.13.11-alpha";
+    const std::string PRMERS_VERSION = "4.13.12-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
