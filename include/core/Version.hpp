0393fc074c1b8ce430bc3f5dcadbf1efd4bed1f2
cherubrock-seb <iofabul@gmail.com>
2025-09-20 15:43:34 +0200

    P-1 factoring in chunk with big B1 bound FASTER BUILD E

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.13.21-alpha";
+    const std::string PRMERS_VERSION = "4.13.22-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
