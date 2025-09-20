87ab3d7d33739f448bf27237caa810110f5fb36c
cherubrock-seb <iofabul@gmail.com>
2025-09-20 19:41:38 +0200

    P-1 factoring in chunk with big B1 bound FASTER integer reconstruct

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.13.25-alpha";
+    const std::string PRMERS_VERSION = "4.13.26-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
