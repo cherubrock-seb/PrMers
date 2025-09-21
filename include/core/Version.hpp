23b1f131ce8bc605eae566d795e0a835906f5c2c
cherubrock-seb <iofabul@gmail.com>
2025-09-21 15:24:06 +0200

    Solved Bug in Gerbicz

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.13.27-alpha";
+    const std::string PRMERS_VERSION = "4.13.28-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
