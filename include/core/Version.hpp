5606624fb6a9d61f80923c174c76f72b7b477cc5
cherubrock-seb <iofabul@gmail.com>
2025-09-28 23:23:56 +0200

    P-1 Factoring STAGE 2 with Marin backend

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.13.38-alpha";
+    const std::string PRMERS_VERSION = "4.13.39-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
