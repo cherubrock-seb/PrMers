1d027e9ba924d8cc2146381447d745d3e9ea9e87
cherubrock-seb <iofabul@gmail.com>
2025-10-01 20:00:29 +0200

    n^K Stage-2

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.14.01-alpha";
+    const std::string PRMERS_VERSION = "4.14.02-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
