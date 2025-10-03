b4a26d35032792528ac9863a903ef8ae31a7f47a
cherubrock-seb <iofabul@gmail.com>
2025-10-03 21:14:09 +0200

    Add ECM algo

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.14.03-alpha";
+    const std::string PRMERS_VERSION = "4.14.04-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
