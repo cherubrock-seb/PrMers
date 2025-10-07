66c663b085dbad2109909902404e7717fb7c3035
cherubrock-seb <iofabul@gmail.com>
2025-10-07 17:25:25 +0200

    LL with errochecking (A = T(a), B = T(b), T^-1(A^2 + T(3)*B^2), 2*T^-1(AB)) (start a (2,1) and check if (-1,0) then if yes its prime) 

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.14.07-alpha";
+    const std::string PRMERS_VERSION = "4.14.08-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
