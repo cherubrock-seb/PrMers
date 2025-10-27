0f4335b648671570cab98afe55eb47f9746451d7
cherubrock-seb <iofabul@gmail.com>
2025-10-27 17:11:27 +0100

    correct bug in restore PM1 state stage 1 with -b2 setted

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.03-alpha";
+    const std::string PRMERS_VERSION = "4.15.04-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
