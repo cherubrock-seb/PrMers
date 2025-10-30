c904a0bfea863d3cf459b743d870cc7f6f3da7cf
cherubrock-seb <iofabul@gmail.com>
2025-10-30 14:13:14 +0100

    ECM montgomery and edwards, torsion 8 and 16

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.04-alpha";
+    const std::string PRMERS_VERSION = "4.15.05-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
