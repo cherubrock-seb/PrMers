8872e56ab9d270dedb69ab3beec0e2a02e7e5eec
cherubrock-seb <iofabul@gmail.com>
2025-11-12 12:33:36 +0100

    ECM Twisted Edwards Compute via -ced parameter (to improve not optimal)

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.19-alpha";
+    const std::string PRMERS_VERSION = "4.15.20-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
