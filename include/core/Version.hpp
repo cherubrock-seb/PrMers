a1b4e7a217124c89f6bf693faaa472958b846e96
cherubrock-seb <iofabul@gmail.com>
2025-11-20 16:16:53 +0100

    Edwards twisted torsion 16

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.23-alpha";
+    const std::string PRMERS_VERSION = "4.15.24-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
