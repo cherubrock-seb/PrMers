c0e76f00196b3bce851dcebc27104fc3ec1274b2
cherubrock-seb <iofabul@gmail.com>
2025-12-03 21:21:46 +0100

    -checksum to recompute checksum of wrong json

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.44-alpha";
+    const std::string PRMERS_VERSION = "4.15.45-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
