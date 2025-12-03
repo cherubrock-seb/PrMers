9fbe221ebb0a4a7036c73429e3cf92885ae22a90
cherubrock-seb <iofabul@gmail.com>
2025-12-03 22:08:37 +0100

    backtrack

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.45-alpha";
+    const std::string PRMERS_VERSION = "4.15.46-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
