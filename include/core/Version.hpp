29bb3dabdfc3aba8021cec8d9b612f7f02afc9fe
cherubrock-seb <iofabul@gmail.com>
2025-12-04 01:29:12 +0100

    @JamesHeinrich version build for #25 Update Json builder JSON os should be a top-level key, not inside program

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.49-alpha";
+    const std::string PRMERS_VERSION = "4.15.50-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
