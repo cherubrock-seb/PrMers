1742a8949e9d06d1ea100b83cb83610ce11490ec
cherubrock <iofabul@gmail.com>
2025-12-21 12:21:26 +0100

    Update Version.hpp

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.62-alpha";
+    const std::string PRMERS_VERSION = "4.15.63-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
