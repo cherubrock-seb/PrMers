a0b9e1a48b0bf3817634d118d5a4476cfec571c5
cherubrock-seb <iofabul@gmail.com>
2025-12-23 19:23:59 +0100

    Better known factor / not known handling in json

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.64-alpha";
+    const std::string PRMERS_VERSION = "4.15.65-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
