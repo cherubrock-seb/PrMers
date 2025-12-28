4a904dc96434fac1933f30420fc6ed21b667c808
cherubrock-seb <iofabul@gmail.com>
2025-12-28 11:56:13 +0100

    proof directory proof and proof-tmp

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.70-alpha";
+    const std::string PRMERS_VERSION = "4.15.71-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
