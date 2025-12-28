71eb31d173e09936f3c7b053465bcce9e1b7a7e0
cherubrock-seb <iofabul@gmail.com>
2025-12-28 23:02:17 +0100

    primenet B2 correction

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.72-alpha";
+    const std::string PRMERS_VERSION = "4.15.73-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
