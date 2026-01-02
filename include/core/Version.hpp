dd776023bf4c8627e7831dc6c2acf44f84cffac3
cherubrock-seb <iofabul@gmail.com>
2026-01-02 01:27:11 +0100

    primenet correction nf status on prp badly

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.76-alpha";
+    const std::string PRMERS_VERSION = "4.15.77-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
