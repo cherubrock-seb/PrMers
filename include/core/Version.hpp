101b3720eaa60cbcb0d3b0f390237d3742da1556
cherubrock-seb <iofabul@gmail.com>
2026-02-11 20:27:04 +0100

    bench

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.84-alpha";
+    const std::string PRMERS_VERSION = "4.15.85-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
