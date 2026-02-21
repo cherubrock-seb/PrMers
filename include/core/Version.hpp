b805f49ead0b188622ed7425172df55c4a208898
cherubrock-seb <iofabul@gmail.com>
2026-02-21 09:51:56 +0100

    ECM ouptut .p95 add PGM + WHO + TIME

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.86-alpha";
+    const std::string PRMERS_VERSION = "4.15.87-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
