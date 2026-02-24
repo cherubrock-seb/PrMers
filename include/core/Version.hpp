3c9eef3144b406e2d0fadafaf5862de19db88aaf
cherubrock-seb <iofabul@gmail.com>
2026-02-24 23:06:04 +0100

    better schedul dbladd dbl ecm

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.95-alpha";
+    const std::string PRMERS_VERSION = "4.15.96-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
