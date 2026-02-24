38f5906b4d305e8c72ee29f21139a2c6b5d82469
cherubrock-seb <iofabul@gmail.com>
2026-02-24 18:05:44 +0100

    fixed muladd engine Marin + use muladd in ecm montgomery faster

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.93-alpha";
+    const std::string PRMERS_VERSION = "4.15.94-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
