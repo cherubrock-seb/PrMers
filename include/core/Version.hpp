7afadca743010ed475c8ab6733a6d50d58e53438
cherubrock-seb <iofabul@gmail.com>
2026-02-25 14:45:24 +0100

    ECM faster create xdbl_tail_uv in marin

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.97-alpha";
+    const std::string PRMERS_VERSION = "4.15.98-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
