1e9d952f76a89f8c38cf72c76a4ae7ecd398c02f
cherubrock-seb <iofabul@gmail.com>
2026-02-25 15:02:15 +0100

    ECM corredct a resume problem after stop

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.15.98-alpha";
+    const std::string PRMERS_VERSION = "4.15.99-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
