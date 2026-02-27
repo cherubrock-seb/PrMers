27aa6a9b7df48fb90a3067e2ccb5cf729618b8aa
cherubrock-seb <iofabul@gmail.com>
2026-02-27 18:11:36 +0100

    Align resume backup ECM

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.16.04-alpha";
+    const std::string PRMERS_VERSION = "4.16.05-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
