e1af820a914837e57332b2dca03626ac7904e98b
cherubrock-seb <iofabul@gmail.com>
2026-02-28 11:05:37 +0100

    ECM no stop in curve 1 with worktodo

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.16.08-alpha";
+    const std::string PRMERS_VERSION = "4.16.09-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
