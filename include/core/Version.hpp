9127e92ba39564bec52798e73ad507825c449c18
cherubrock-seb <iofabul@gmail.com>
2026-03-27 18:16:37 +0100

    Connect mprime and prmers better in P-1 FACTORING

--- include/core/Version.hpp
+++ include/core/Version.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace core {
-    const std::string PRMERS_VERSION = "4.18.09-alpha";
+    const std::string PRMERS_VERSION = "4.19.00-alpha";
 } // namespace core
 
 #endif // VERSION_HPP
